acdc3b49cd65f298d9db4613806c12812278067b
Eray BAHÇEGÜLÜ <eraybahcegulu@gmail.com>
2024-09-02 21:33:49 +0300

    useLanguageStore

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,12 +1,14 @@
 import { useEffect, useState } from 'react'
-import { navItems } from '../locales/tr'
+
 import { FiMenu } from 'react-icons/fi';
 import { Drawer } from 'antd';
 import { MdOutlineKeyboardArrowRight } from 'react-icons/md';
+import { useLanguageStore } from '../stores/LanguageStore';
+import { navItems } from '../constants/strings';
 
 const Navigation = () => {
     const [open, setOpen] = useState(false);
-
+    const { language  } = useLanguageStore();
     const showDrawer = () => {
         setOpen(true);
     };
@@ -32,7 +34,7 @@ const Navigation = () => {
     return (
         <div className='max-w-full text-nowrap xl:text-sm text-xs font-bold'>
             <ul className='hidden xl:flex flex-row md:gap-10'>
-                {navItems.map(({ name }) => (
+            {navItems[language].map(({ name }) => (
                     <span>
                         <li className='cursor-pointer'>
                             {name}
@@ -55,7 +57,7 @@ const Navigation = () => {
                         <MdOutlineKeyboardArrowRight className='text-2xl cursor-pointer hover:scale-125 transition-all' onClick={closeDrawer} />
                     </div>
                     <ul className='flex flex-col gap-5 '>
-                        {navItems.map(({ name }) => (
+                    {navItems[language].map(({ name }) => (
                             <span>
                                 <li className='text-base flex items-center justify-center cursor-pointer hover:text-black hover:bg-white py-4 px-20 rounded-xl'>
                                     {name}
